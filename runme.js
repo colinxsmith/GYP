b24ce889dbc28437cb54b088395f10297cb061ca
Colin Smith <colin.smith@corfinancialgroup.com>
2018-06-15 15:08:24 +0100

    show how to export the exports

--- runme.js
+++ runme.js
@@ -1,30 +1,19 @@
 var test = require("./build/Release/example");
 console.log(test);
-//var http = require('http');
-
-exports.here=function() {
+Object.keys(test).forEach(function (key) {
+    exports[key] = test[key];
+});
+exports.here = function () {
     return 'Inside runme';
-}
-
-
-var a = [1.0,2.0,3.0,4.0],b = [1,1,1,1];
-
-
-var dot = test.opt(a.length,a,b);
+};
+console.log(exports);
+var a = [1.0, 2.0, 3.0, 4.0], b = [1, 1, 1, 1];
+var dot = test.opt(a.length, a, b);
 console.log(dot);
 console.log(b);
-
 console.log(test.Return_Message(6));
 console.log(test.version());
-
-var idot = test.iopt(b.length,b,b);
+var idot = test.iopt(b.length, b, b);
 console.log(idot);
 console.log(b);
-
-console.log('Using optimiser for this: '+test.ddotvec(a.length,a,b));
-
-/* http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(test.version());
-    res.end();
-}).listen(8080); */
+console.log('Using optimiser for dot: ' + test.ddotvec(a.length, a, b));
